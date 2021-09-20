6e851e0773fc5929502a4ae4357e28a725c3b320
Pete Montgomery <sumo.the.cat@gmail.com>
2021-09-20 15:23:16 +0100

    Convert /stats to use example Athena query

--- api/src/endpoints/stats.ts
+++ api/src/endpoints/stats.ts
@@ -1,22 +1,69 @@
 
-import * as format from 'pg-format'
 
-import { MonthlyQuery } from '../query'
-import { query } from '../db'
+// example: POST /stats
+// {
+//     "framework": "liveng0",
+//     "indexname": "NDVI",
+//     "poly_parition": ["SD87", "SD88"],
+//     "polyid": ["489639", "489640", "489647", "489658"]
+// }
 
-export const getStats = async (q: MonthlyQuery) => {
-    console.log(q)
+import { athenaExpress } from "../aws"
 
-    let sql = format(`
-        select *
-        from %I
-        where year=$1 and month=$2
-        limit 1000`,
-        'polygon_stats_by_month')
+export const getStats = async (args: any) => {
 
-    let result = await query(
-        sql,
-        [q.year, q.month])
-        
-    return result.rows
+    console.log(`At ${(new Date()).toISOString()} - entering function`)
+
+    let sql = `
+        select s.polyid,
+            max(s.z_mean_abs)   as z_mean_abs,
+            max(s.z_median_abs) as z_median_abs,
+            max(s.z_min_abs)    as z_min_abs,
+            max(s.z_max_abs)    as z_max_abs,
+            max(s.z_q1_abs)     as z_q1_abs,
+            max(s.z_q3_abs)     as z_q3_abs
+        from stats_compared_monthly_nearest50_10km s
+        where
+            framework='liveng0'
+            and indexname='NDVI'
+            and poly_partition in ('SD87', 'SD88')
+            and polyid in ('489639', '489640', '489647', '489658')
+        group by polyid
+        limit 10
+        `
+
+    let query = { sql, db: "statsdb" }
+    let result = await athenaExpress().query(query)
+
+    console.log(`At ${(new Date()).toISOString()} - got query result`)
+    
+    return {
+        data: result.Items
+    }
 }
+
+
+
+
+
+// import * as format from 'pg-format'
+
+// import { MonthlyQuery } from '../query'
+// import { query } from '../db'
+
+// export const getStats = async (q: MonthlyQuery) => {
+//     console.log(q)
+
+//     let sql = format(`
+//         select *
+//         from %I
+//         where year=$1 and month=$2
+//         limit 1000`,
+//         'polygon_stats_by_month')
+
+//     let result = await query(
+//         sql,
+//         [q.year, q.month])
+        
+//     return result.rows
+// }
