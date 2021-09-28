217e75cdc949d0246f4b667b707c2b72fdc74c2b
Pete Montgomery <sumo.the.cat@gmail.com>
2021-09-28 16:24:56 +0100

    Demo react-leaflet

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,22 +1,10 @@
 
-import React, { useState } from 'react'
+import React from 'react'
 import { LeafletMap } from './LeafletMap'
 
 export function App() {
-  const [count, setCount] = useState(0)
   return (
-
-    <>
-    <h1 className="text-4xl text-jncc">Welcome to here</h1>
-    <div className="p-6">
-      <img src="http://placekitten.com/200/200" height="200" width="200" alt="Kitteh" />
-    </div>
-
-    <button type="button" onClick={() => setCount((count) => count + 1)}>
-      Counter {count}
-    </button>
-    </>
-    // <LeafletMap />
+    <LeafletMap />
   )
 }
 
