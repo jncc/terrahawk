da744888608930f5b38eadadbeb604cc8f9221c4
CathyJinJNCC <cathy.jin@jncc.gov.uk>
2022-01-24 10:32:14 +0000

    Undoing vite changes

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -7,16 +7,6 @@ const config: UserConfig = {
   // server: {
     //fs: { strict: true } // this will become default in future versions of vite, so can remove soon
   // }
-
-  // watch the thumbnails package
-  server: {
-    watch: {
-      ignored: ['!**/node_modules/thumbnail-generator/**']
-    }
-  },
-  optimizeDeps: {
-    exclude: ['thumbnail-generator']
-  }
 };
 
 export default config;
