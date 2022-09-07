3d620c801724c80077828f0f0f4aa62e4fe18fa3
CathyJinJNCC <cathy.jin@jncc.gov.uk>
2022-09-07 17:14:26 +0100

    Framework panel tweaks

--- www/src/slices/mapper/FrameworkPanel.tsx
+++ www/src/slices/mapper/FrameworkPanel.tsx
@@ -11,30 +11,27 @@ export let FrameworkPanel = () => {
   let dispatch = useStateDispatcher()
   let query = useStateSelector(s => s.mapper.query)
 
-  let framework = frameworks[query.framework]
-
   return (
     <Panel extraClasses="absolute bottom-4 left-6 min-w-[14rem] pl-4 pr-6 py-2 my-2">
       <div className="flex items-center space-x-3">
         <MapIcon className="h-7 w-7 text-gray-400"/>
-        <div className="leading-tight">
-          {/* <div>{framework.name}</div>
-          <div className="flex gap-2 items-center">
+        <div className="leading-tight">            
+          <select
+            name="frameworkname" id="frameworkname-select"
+            defaultValue={query.framework}
+            onChange={e => dispatch(mapperActions.alterQueryFramework(e.target.value))}
+            className="bg-white hover:bg-gray-200 p-0.5"
+            >
+            {
+              Object.entries(frameworks).map(([name, framework]) => <option key={name} value={name} selected={name==query.framework}>{`${framework.name}`}</option>)
+            }
+          </select>
+
+          <div className="flex gap-2 items-center ml-1.5">
             <div className="little-label-text ">{query.framework}</div>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-1 w-7 " stroke="#ff7800">
               <line x1="0" y1="3" x2="60" y2="3" strokeWidth="3" strokeDasharray="5 5" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
-          </div> */}
-          <div>
-            <select
-              name="frameworkname" id="frameworkname-select"
-              defaultValue={query.framework}
-              onChange={e => dispatch(mapperActions.alterQueryFramework(e.target.value))}
-              >
-              {
-                Object.entries(frameworks).map(([name, framework]) => <option key={name} value={name} selected={name==query.framework}>{`${framework.name}`}</option>)
-              }
-            </select>
           </div>
         </div>
       </div>
