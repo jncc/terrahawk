e55d4569e3e54763f49c0985a24f3cfb8b7b2d42
martin bamford <martin.bamford@jncc.gov.uk>
2023-01-04 16:41:24 +0000

    New lambda to get all habitats per framework

---
+++ api/src/endpoints/habitats.ts
@@ -0,0 +1,39 @@
+
+import { query } from '../db'
+import { parseArgs } from './habitatsArgParser'
+
+/*
+    example: POST /habitats
+    {
+        "framework": "liveng0"
+    }
+*/
+
+export const getHabitats = async (args: any) => {
+
+    console.log(`At ${(new Date()).toISOString()} - entering function`)
+
+    let q = parseArgs(args)    
+
+    let habitats = await getHabitatsImpl(q)
+    
+    console.log(`At ${(new Date()).toISOString()} - got query result`)
+
+    return { habitats }
+}
+
+export let getHabitatsImpl = async (q: { framework: string }) => {
+    
+    let sql = `
+      select
+         id,
+         habitat
+      from habitats
+      where framework = $1
+      limit 999
+      `
+
+    let habitatRows = await query(sql, [q.framework])
+
+    return habitatRows.rows
+}
