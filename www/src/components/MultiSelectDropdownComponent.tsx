6e6092300703daebbb1e00bb205399ad2bee07c8
martin bamford <martin.bamford@jncc.gov.uk>
2023-01-20 17:00:06 +0000

    Add spacing around checkboxes

--- www/src/components/MultiSelectDropdownComponent.tsx
+++ www/src/components/MultiSelectDropdownComponent.tsx
@@ -26,18 +26,17 @@ export let MultiSelectDropdown = (props: {options: Array<Option>, selected: Arra
               <button type="button" className="c-multi-select-dropdown__button" onClick={() => apply ( props.applyFunction)}>Apply</button>
               <img src={dropdown} onClick={() => toggleOptionsDisplayed ()}/>
           </div>
-          <ul className="c-multi-select-dropdown__options">
-            <li className="c-multi-select-dropdown__option" key='0' 
-                onClick={() => props.toggleAllFunction((document.getElementById("selectAllCheckbox") as HTMLInputElement).checked)}>
+          <ul className="h-9 p-1 w-full border-2 border-gray-300 text-gray-900 rounded-lg custom-ring c-multi-select-dropdown__options">
+            <li key='0' onClick={() => props.toggleAllFunction((document.getElementById("selectAllCheckbox") as HTMLInputElement).checked)}>
                 <input id="selectAllCheckbox" type="checkbox" readOnly></input>
-                <span>Select All</span>
+                <span className="c-multi-select-dropdown__option">Select All</span>
             </li>   
             {props.options.map(option => {
                 const isSelected = props.selected.includes(option.id);
                 return (
                     <li key={option.id} onClick={() => props.toggleItemFunction(option.id)}>
                         <input type="checkbox" checked={isSelected} readOnly></input>
-                        <span>{option.title}</span>
+                        <span className="c-multi-select-dropdown__option">{option.title}</span>
                     </li>
                 )
             })}
