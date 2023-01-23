4a40ba70c74c6e240c87c0b911a674a26101f89f
martin bamford <martin.bamford@jncc.gov.uk>
2023-01-23 16:30:21 +0000

    Use the same down arrow icon as is used in standard widgets

--- www/src/components/MultiSelectDropdownComponent.tsx
+++ www/src/components/MultiSelectDropdownComponent.tsx
@@ -1,6 +1,6 @@
-import dropdown from './dropdown.png';
 import './MultiSelectDropdown.css';
 import React, { useEffect } from 'react'
+import { ChevronDownIcon } from '@heroicons/react/solid'
 
 type Option = {
     id: number
@@ -35,9 +35,11 @@ export let MultiSelectDropdown = (props: {options: Array<Option>, selected: Arra
     return (
           <div className="c-multi-select-dropdown">
           <div className="h-9 p-1 w-full border-2 border-gray-300 text-gray-900 rounded-lg custom-ring bg-gray-200 c-multi-select-dropdown__selected">      
-              <div>{props.selected.length} selected</div>   
-              <button type="button" className="c-multi-select-dropdown__button" onClick={() => apply ( props.applyFunction)}>Apply</button>
-              <img src={dropdown} onClick={() => toggleOptionsDisplayed ()}/>
+            <div>{props.selected.length} selected</div>
+            <button type="button" className="c-multi-select-dropdown__button" onClick={() => apply ( props.applyFunction)}>Apply</button>   
+            <button className="right-0 mr-0" onClick={() => toggleOptionsDisplayed ()}>
+                <ChevronDownIcon className="h-4 w-4"/>
+            </button>
           </div>
           <ul className="h-9 p-1 w-full border-2 border-gray-300 text-gray-900 rounded-lg custom-ring bg-gray-50 c-multi-select-dropdown__options">
             <li key='0' onClick={() => props.toggleAllFunction((document.getElementById("selectAllCheckbox") as HTMLInputElement).checked)}>
