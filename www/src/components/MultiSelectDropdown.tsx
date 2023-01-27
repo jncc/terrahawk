38e841924cc851b426492fde2e6fd47104aae75c
martin bamford <martin.bamford@jncc.gov.uk>
2023-01-27 09:14:09 +0000

    Remove separate css file using tailwind clases or main css file as appropriate

--- www/src/components/MultiSelectDropdown.tsx
+++ www/src/components/MultiSelectDropdown.tsx
@@ -1,15 +1,9 @@
-import './MultiSelectDropdown.css';
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ChevronDownIcon } from '@heroicons/react/solid'
 
 type Option = {
     id: number
     title: string
-}
-
-function toggleOptionsDisplayed(){
-    var currentStyle = getComputedStyle(document.documentElement).getPropertyValue('--options-display');
-    document.documentElement.style.setProperty('--options-display', currentStyle === 'block' ? 'none' : 'block');
 }
 
 function ensureSelectAllConformity(totalOptions: number, totalSelected: number){
@@ -22,35 +16,42 @@ function ensureSelectAllConformity(totalOptions: number, totalSelected: number){
 }
 
 export let MultiSelectDropdown = (props: {options: Array<Option>, selected: Array<number>, toggleItemFunction: Function, toggleAllFunction: Function}) => {
+    const [isOptionsDisplayed, setOptionsDisplayed] = useState(false);
+
+    function toggleOptionsDisplayed(){
+        setOptionsDisplayed(!isOptionsDisplayed)
+    }
 
     useEffect(() => {
         ensureSelectAllConformity(props.options.length, props.selected.length)
     }, [props.selected])
     
     return (
-        <div className="c-multi-select-dropdown">
-            <div className="h-9 p-1 w-full border-2 border-gray-300 text-gray-900 rounded-lg custom-ring bg-gray-100 c-multi-select-dropdown__selected"
+        <div className="relative">
+            <div className="h-9 p-1 w-full border-2 border-gray-300 text-gray-900 rounded-lg custom-ring bg-gray-100 flex justify-between"
                 onClick={() => toggleOptionsDisplayed ()}>      
                 <div>{props.selected.length} selected</div> 
                 <button className="right-0 mr-0">
                     <ChevronDownIcon className="h-5 w-5"/>
                 </button>
             </div>
-            <ul className="h-9 p-1 w-full border-2 border-gray-300 text-gray-900 rounded-lg custom-ring bg-gray-50 c-multi-select-dropdown__options">
-                <li key='0' onClick={() => props.toggleAllFunction((document.getElementById("selectAllCheckbox") as HTMLInputElement).checked)}>
-                    <input id="selectAllCheckbox" type="checkbox" readOnly></input>
-                    <span className="c-multi-select-dropdown__option">Select All</span>
-                </li>   
-                {props.options.map(option => {
-                    const isSelected = props.selected.includes(option.id);
-                    return (
-                        <li key={option.id} onClick={() => props.toggleItemFunction(option.id)}>
-                            <input type="checkbox" checked={isSelected} readOnly></input>
-                            <span className="c-multi-select-dropdown__option">{option.title}</span>
-                        </li>
-                    )
-                })}
-            </ul>
+            <div className={isOptionsDisplayed ? 'block' : 'hidden'}>
+                <ul className="p-1 w-full border-2 border-gray-300 text-gray-900 rounded-lg custom-ring bg-gray-50 overflow-y-scroll c-multi-select-dropdown__options">
+                    <li key='0' onClick={() => props.toggleAllFunction((document.getElementById("selectAllCheckbox") as HTMLInputElement).checked)}>
+                        <input id="selectAllCheckbox" type="checkbox" readOnly></input>
+                        <span className="ml-1">Select All</span>
+                    </li>   
+                    {props.options.map(option => {
+                        const isSelected = props.selected.includes(option.id);
+                        return (
+                            <li key={option.id} onClick={() => props.toggleItemFunction(option.id)}>
+                                <input type="checkbox" checked={isSelected} readOnly></input>
+                                <span className="ml-1">{option.title}</span>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
