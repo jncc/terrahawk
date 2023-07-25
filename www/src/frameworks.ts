595951db11e76a71bbb9b40ba529e5d28021dfc7
Felix Mason <felix.mason@jncc.gov.uk>
2023-07-25 13:30:32 +0100

    removed liveng0 from framework list

--- www/src/frameworks.ts
+++ www/src/frameworks.ts
@@ -7,32 +7,32 @@ import liveng1Boundary from './assets/frameworks/liveng1-boundary.json'
 import cairngormsBoundary from './assets/frameworks/cairngorms-boundary.json'
 
 export const frameworks: {[framework: string]: Framework} = {
-  liveng0: {
-    name: `Living England: Yorkshire subset`,
-    srs: `osgb`,
-    boundary: liveng0Boundary,
-    maxBounds: {northEast: {lat: 60, lng: 3}, southWest: {lat: 48, lng: -11}},
-    defaultZoom: 9,
-    bboxPadding: {latPad: 0.03, lngPad: 0.06},
-    maxZoom: 18,
-    minZoom: 7,
-    // defaultZoom: 13,
-    polygonZoomThreshold: 13,
-    defaultQuery: {
-      tableName: 'liveng0',
-      center: {lat: 54, lng: -1.4},
-      // center: {lat: 53.7, lng: -0.45},
-      indexname: 'NDVI',
-      statistic: 'mean',
-      yearFrom: 2020,
-      monthFrom: 1,
-      yearTo: 2021,
-      monthTo: 12,
-      habitatids: [],
-    },
-    availableIndices: ['NBR', 'NDMI', 'NDVI', 'NDWI']
-  }
-  ,
+  // liveng0: {
+  //   name: `Living England: Yorkshire subset`,
+  //   srs: `osgb`,
+  //   boundary: liveng0Boundary,
+  //   maxBounds: {northEast: {lat: 60, lng: 3}, southWest: {lat: 48, lng: -11}},
+  //   defaultZoom: 9,
+  //   bboxPadding: {latPad: 0.03, lngPad: 0.06},
+  //   maxZoom: 18,
+  //   minZoom: 7,
+  //   // defaultZoom: 13,
+  //   polygonZoomThreshold: 13,
+  //   defaultQuery: {
+  //     tableName: 'liveng0',
+  //     center: {lat: 54, lng: -1.4},
+  //     // center: {lat: 53.7, lng: -0.45},
+  //     indexname: 'NDVI',
+  //     statistic: 'mean',
+  //     yearFrom: 2020,
+  //     monthFrom: 1,
+  //     yearTo: 2021,
+  //     monthTo: 12,
+  //     habitatids: [],
+  //   },
+  //   availableIndices: ['NBR', 'NDMI', 'NDVI', 'NDWI']
+  // }
+  // ,
   liveng1: {
     name: `Living England`,
     srs: `osgb`,
